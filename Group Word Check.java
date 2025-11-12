6f54eb339d77fbce19a8092bc05102e7332404c2
junygdev-ctrl <junyg.dev@gmail.com>
2025-11-12 13:02:28 +0900

    Update Group Word Check.java

--- Group Word Check.java
+++ Group Word Check.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class GroupWordCheck {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
